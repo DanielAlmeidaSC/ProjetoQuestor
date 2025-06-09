a006ca954133c1d0bae9e94a7062bf773a242a24
Daniel Almeida <eexpeditodaniel@gmail.com>
2025-06-09 16:59:44 -0300

    Criação de endpoint post para boleto

---
+++ Questor/Endpoints/BoletoPost.cs
@@ -0,0 +1,41 @@
+﻿using Questor.Database;
+using Questor.Entities;
+
+namespace Questor.Endpoints
+{
+    public class BoletoPost
+    {
+        public static string Caminho => "/boletos";
+        public static string[] Metodos => new string[] { HttpMethod.Post.ToString() };
+        public static Delegate Comportamento => Action;
+
+        public static async Task<IResult> Action(Boleto boletoInsert, ApplicationDbContext db)
+        {
+            Boleto boleto = new Boleto
+            {
+                NomePagador = boletoInsert.NomePagador,
+                CpfCnpjPagador = boletoInsert.CpfCnpjPagador,
+                CpfCnpjBeneficiario = boletoInsert.CpfCnpjBeneficiario,
+                Valor = boletoInsert.Valor,
+                DataVencimento = boletoInsert.DataVencimento,
+                Observacao = boletoInsert.Observacao,
+                BancoId = boletoInsert.BancoId
+                
+            };
+
+            if (!boleto.IsValid)
+            {
+                var erro = boleto.Notifications.GroupBy(b => b.Key)
+                    .ToDictionary(n => n.Key,
+                    b => b.Select(n => n.Message).ToArray());
+
+                return Results.ValidationProblem(erro);
+            }
+
+            await db.Boleto.AddAsync(boleto);
+            await db.SaveChangesAsync();
+
+            return Results.Created($"/boletos/{boleto.Id}", boleto.Id);
+        }
+    }
+}
