7c7247ab5d467a0f26fb1ddb4eab7e3b4ee8bb8c
Daniel Almeida <eexpeditodaniel@gmail.com>
2025-06-09 09:48:23 -0300

    Criação das entidades Boleto e Banco

---
+++ Questor/Entities/Banco.cs
@@ -0,0 +1,34 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.Contracts;
+
+namespace Questor.Entities
+{
+    public class Banco : Notifiable<Notification>
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int CodigoBanco { get; set; }
+        public int PercentualJuros { get; set; }
+
+        public Banco(int id, string nome, int codigoBanco, int percentualJuros)
+        {
+            Id = id;
+            Nome = nome;
+            CodigoBanco = codigoBanco;
+            PercentualJuros = percentualJuros/100;
+
+            var contract = new Contract<Banco>()
+                .Requires()
+                .IsNotNullOrEmpty(Nome, "Nome", "O nome do banco é um requisito obrigatório e não pode ficar vazio ou nulo!")
+                .IsGreaterThan(CodigoBanco, 0, "CodigoBanco", "O código do banco é um requisito obrigatório e deve ser maior que 0")
+                .IsGreaterThan(PercentualJuros, 0, "PercentualJuros", "O percentual de juros é um requisito obrigatório e deve ser maior que 0");
+
+            AddNotifications(contract);
+        }
+    }
+}
---
+++ Questor/Entities/Boleto.cs
@@ -0,0 +1,46 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.Contracts;
+
+namespace Questor.Entities
+{
+    public class Boleto : Notifiable<Notification>
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string NomePagador { get; set; }
+        public string CpfCnpjPagador { get; set; }
+        public string NomeBeneficiario { get; set; }
+        public string CpfCnpjBeneficiario { get; set; }
+
+        public double Valor { get; set; }
+        public DateTime DataVencimento { get; set; }
+        public string Observacao{ get; set; }
+        public int BancoId { get; set; }
+
+        public Boleto(string nomePagador, string cpfCnpjPagador, string nomeBeneficiario, string cpfCnpjBeneficiario, double valor, DateTime dataVencimento, string observacao int bancoId)
+        {
+            NomePagador = nomePagador;
+            CpfCnpjPagador = cpfCnpjPagador;
+            NomeBeneficiario = nomeBeneficiario;
+            CpfCnpjBeneficiario = cpfCnpjBeneficiario;
+            Valor = valor;
+            DataVencimento = dataVencimento;
+            Observacao = observacao;
+
+            var contract = new Contract<Boleto>()
+                .Requires()
+                .IsNotNullOrEmpty(NomePagador, "NomePagador", "O nome do pagador é um requisito obrigatório e não pode ficar vazio ou nulo!")
+                .IsNotNullOrEmpty(NomeBeneficiario, "NomeBeneficiario", "O nome do beneficiário é um requisito obrigatório e não pode ficar vazio ou nulo!")
+                .IsNotNullOrEmpty(CpfCnpjPagador, "CPF/CNPJPagador", "O CPF/CNPJ do pagador é um requisito obrigatório e não pode ficar vazio ou nulo!")
+                .IsNotNullOrEmpty(CpfCnpjBeneficiario, "CPF/CNPJBeneficiario", "O CPF/CNPJ do beneficiário é um requisito obrigatório e não pode ficar vazio ou nulo!")
+                .IsGreaterThan(Valor, 0.0, "Valor", "O valor do boleto é um requisito obrigatório e deve ser maior que R$ 0,00");
+
+            AddNotifications(contract);
+        }
+    }
+}
